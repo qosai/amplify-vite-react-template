9b106ac8dcd04d8ce8331e79fcfcc9542ca88b31
qosai <qosai.samara@gmail.com>
2025-10-22 05:02:02 +0300

    Add S3 Storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,7 +2,7 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'appUserFiles',
+    name: 'qosai_amplify_appUserFiles',
     isDefault: true,
     access: (allow) => ({
         // Authenticated users can list/read/write/delete under this prefix
